ce8e5cee67544ff0426a428a8e85310af371147e
lizhenbin <lizhenbin@oneplus.cn>
2016-01-13 21:16:21 +0800

    重构

---
+++ auto/git-auto-tag.py
@@ -0,0 +1,43 @@
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+__author__ = 'Zhenbin.Li'
+
+import os
+from time import strftime, localtime
+
+# WMS版本号
+TAG_VERSION = 'wmsV3.6.0.';
+
+
+# 初始化git tag文件名称
+def initTag():
+    dateStr = strftime('%Y%m%d%H%M', localtime())
+    return TAG_VERSION + dateStr;
+
+
+# 执行git命令生成tag并且上传到git
+def doTag():
+    try:
+        os.popen("git pull")
+        print 'git pull finish.'
+
+        os.popen("git push")
+        print 'git pull finish.'
+
+        tag = initTag()
+        cmd = "git tag -a " + tag + " -m " + "\"" + tag + "\""
+        os.system(cmd)
+
+        os.popen("git push origin --tags")
+        print 'git pull tag finish.'
+        print "tag:", tag
+
+    except:
+        print 'local auto git tag error.'
+        return
+
+
+if __name__ == '__main__':
+    print 'auto git tag start...'
+    doTag()
+    print 'auto git tag finish'
